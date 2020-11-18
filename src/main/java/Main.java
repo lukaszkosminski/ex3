94618589ad75fb7d0ce289455d3a29e23e357cc5
lukaszkosminski <dnb8@o2.pl>
2020-11-18 15:35:46 +0100

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] myArray2 = new String[0];
+
+        if (myArray2.length == 0) {
+            System.out.println(myArray2.length - 1);
+
+        } else {
+            System.out.println(myArray2.length);
+        }
+    }
+}
+
+
